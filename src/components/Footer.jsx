5c4bf403ae81f671f297253aa35ea6f4ec4c4d8e
maltin123 <maltin123@users.noreply.github.com>
2026-08-25 22:00:23 +0630

    Update resume, add second phone number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -63,26 +63,35 @@ export default function Footer({ simple }) {
                   mansitt1997@gmail.com
                 </a>
               </li>
-              <li>
-                <a
-                  href="tel:+959441093130"
-                  className="text-muted text-sm hover:text-accent transition-colors duration-300 inline-flex items-center gap-2"
+              <li className="text-muted text-sm inline-flex items-center gap-2">
+                <svg
+                  className="w-4 h-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
                 >
-                  <svg
-                    className="w-4 h-4"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                  />
+                </svg>
+                <span>
+                  <a
+                    href="tel:+959441093130"
+                    className="hover:text-accent transition-colors duration-300"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                    />
-                  </svg>
-                  +959-441093130
-                </a>
+                    +959-441093130
+                  </a>
+                  {", "}
+                  <a
+                    href="tel:+959974811616"
+                    className="hover:text-accent transition-colors duration-300"
+                  >
+                    +959-974811616
+                  </a>
+                </span>
               </li>
             </ul>
           </div>
